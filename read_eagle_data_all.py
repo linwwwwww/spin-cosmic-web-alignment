d586255a1d59ca2fa35ddc6e8db400dc5d01cd35
linwwwwww <452555351@qq.com>
2020-09-25 20:23:23 +0800

    add some code from Mac

--- read_eagle_data_all.py
+++ read_eagle_data_all.py
@@ -6,17 +6,16 @@ from multiprocessing import Pool
 import multiprocessing
 from tqdm import trange, tqdm
 import time
-time_start=time.time()
+
 
 #read data
 r200=np.zeros([1])
 mass=np.zeros([1])
 pos=np.zeros([1,3])
 GN=np.zeros([1])
-SGN=np.zeros([1])
 Vel=np.zeros([1,3])
 starmass=np.zeros([1])
-
+FirstID=np.zeros([1])
 for i in np.arange(256):
     f = h5py.File('/Simulations/Eagle/RefL0100N1504/groups_028_z000p000/eagle_subfind_tab_028_z000p000.'+np.str(i)+'.hdf5', 'r')
     r200=np.concatenate((r200,f['FOF']['Group_R_Crit200']))
@@ -25,46 +24,41 @@ for i in np.arange(256):
     mass=np.concatenate((mass,f['FOF']['Group_M_Crit200']))
     GroupNumber=f["Subhalo"]["GroupNumber"]
     GN=np.concatenate((GN,GroupNumber))
-    SubGroupNumber=f["Subhalo"]["SubGroupNumber"]
-    SGN=np.concatenate((SGN,SubGroupNumber))
+
     Vel=np.concatenate((Vel,f["Subhalo"]["Velocity"])) 
     starmass=np.concatenate((starmass,f['Subhalo']['Stars']['Mass']))    
-    
-    
+    FirstID=np.concatenate((FirstID,f['FOF']['FirstSubhaloID']))
+    f.close()
 r200 = np.delete(r200, 0, 0)
 Vel = np.delete(Vel, 0, 0)
 pos = np.delete(pos,0,0)
 mass = np.delete(mass,0,0)
 GN = np.delete(GN,0,0)
-SGN = np.delete(SGN,0,0)
 starmass=np.delete(starmass,0,0)
+FirstID=np.delete(FirstID,0,0)
 
 
+Vel=Vel[np.int32(FirstID)]#选择中心halo
+starmass=starmass[np.int32(FirstID)]
 
-main=np.int32(np.where(SGN==0))
-GN=GN[main]
-Vel=Vel[main] 
-starmass=starmass[main]
-
-
-GN=np.int32(GN-1)
-
-GN=np.int32(GN)
-r200=r200[GN]
-pos=pos[GN]
-mass=mass[GN] 
-
-num=(np.where(mass>=1e-2))
+num=(np.where(mass>=1))#M200>10^10 M_sun
 r200=r200[num]
 pos=pos[num]
 mass=mass[num]
 Vel=Vel[num]
 starmass=starmass[num]
 
-np.save("starmass.npy",starmass)
+select=(np.where(starmass>=1e-1))#stellar mass>10^9 M_sun
+r200=r200[select]
+pos=pos[select]
+mass=mass[select]
+Vel=Vel[select]
+starmass=starmass[select]
+
+np.save("stellarmass1e-1_20920_multi.npy",starmass)
 
 print("finish 1")
-
+print("len(mass)=%s\n"%(len(mass)))
 
 #use the particle data to compute spin
 
@@ -72,10 +66,10 @@ basePath= '/Simulations/Eagle/RefL0100N1504/'
 snapnum=28
 
 
-def parts_in_region(basePath,snapnum, partType, centre: 'cMpc/h', region_length:'cMpc/h', fields,j):
+def parts_in_region(basePath,snapnum, partType, centre: 'cMpc/h', region_length:'cMpc/h', fields):
     #print('\n##\n#region_length is box\'s length of selected region, not radius',)
     #fname   = basePath+"/snapshot_0%d_z000p000/snap_0%d_z000p000.0.hdf5" % (snapnum, snapnum)
-    fname   = eagle.snapshot.snapPath(basePath,snapnum,chunkNum=j)
+    fname   = eagle.snapshot.snapPath(basePath,snapnum)
     itype = partType
     result = {}
     # make sure fields is not a single element
@@ -92,7 +86,8 @@ def parts_in_region(basePath,snapnum, partType, centre: 'cMpc/h', region_length:
     zmin = centre[2]- 0.5*region_length
     zmax = centre[2]+ 0.5*region_length
     snap.select_region(xmin, xmax, ymin, ymax, zmin, zmax)
-
+    
+    
     #print ("# Number of particles in this region = %d" % snap.count_particles(itype))
     # Read positions and IDs of particles of type itype in the specified region.
     for field in fields:
@@ -103,30 +98,27 @@ print("finish 2")
 
 parttype=1
 cores = multiprocessing.cpu_count()
-print("len(mass)=%s\n"%(len(mass)))
+
 def Jcompute(i):
     print(i/len(mass))
-    J=np.zeros([1,3])    
-    for j in range(256):
-        halop=parts_in_region(basePath,snapnum,parttype,pos[i],r200[i],['Coordinates','Velocity'],j)        
-        halopr=halop['Coordinates']-pos[i]
-        halopv=halop['Velocity']-Vel[i]
-        Jall=np.cross(halopr,halopv)
-        Jallsum=Jall.sum(axis=0)
-        J=np.vstack((J,Jallsum))
-    J=np.delete(J,0,0)
-    J=J.sum(axis=0)
-    
-    if(i%10000)==0:
-        timenow=time.time()
-        print("-----------------now--------------")
-        print('time cost',time_end-time_start,'s')
+
+    halop=parts_in_region(basePath,snapnum,parttype,pos[i],r200[i],['Coordinates','Velocity'])      
+    halopr=halop['Coordinates']-pos[i]
+    halopv=halop['Velocity']-Vel[i]
+    halop=halop[np.where(np.linalg.norm(halopr, ord = 2)<r200[i])]
+
+    r3d=np.sqrt(np.sum(halopr*halopr, axis = 1))
+    halopr=halopr[np.where(r3d < r200[i]),:][0]
+    halopv =halopv[np.where(r3d < r200[i]),:][0]
+    Jall=np.cross(halopr,halopv)
+    J=Jall.sum(axis=0)
     
     return J
 if __name__ == '__main__':
     cores = multiprocessing.cpu_count()
     with Pool(cores) as p:
         J=p.map(Jcompute, range(len(mass)))
+
 print("finish 3")
 Galneedtype=[
     ( 'Pos'                  , (np.float32, 3)), 
@@ -138,4 +130,4 @@ halo['Pos']=pos
 halo["Spin"]=J
 halo['Mass']=mass
 
-np.save("halo_pos_J_mass.npy",halo)
+np.save("halo_pos_J_mass_1_stellarmass1e-1_20920_multi.npy",halo)
